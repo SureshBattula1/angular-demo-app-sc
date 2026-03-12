efba6ed8ce456516caf381258797607bacb57e89
murali <nmurali.ab@gmail.com>
2026-03-12 21:39:25 +0530

    docker setup done

---
+++ src/environments/environment.docker.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  environment: 'docker',
+  apiUrl: '/api',
+  appName: 'School Management System',
+  version: '1.0.0',
+  enableDebugTools: false,
+  logLevel: 'info'
+};
