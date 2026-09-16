a73cb98cb35d28a3fc095c138a1a30603d469bdd
Murali <55926762+murali-ab-laravel@users.noreply.github.com>
2026-09-16 18:45:56 +0530

    Change API URL in production environment
    Updated the API URL for production environment.

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   environment: 'production',
-  apiUrl: 'https://demo.schools.api.techtrendstalks.com/api',
+  apiUrl: 'http://54.226.146.86/api',
   appName: 'School Management System',
   version: '1.0.0',
   enableDebugTools: false,  // Disable debug tools in production
