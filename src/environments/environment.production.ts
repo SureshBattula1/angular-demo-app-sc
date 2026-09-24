09a04df9645e488c88fa9901d9ff64debca4f3d7
Murali <55926762+murali-ab-laravel@users.noreply.github.com>
2026-09-24 20:50:33 +0530

    Update API URL for production environment

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   environment: 'production',
-  apiUrl: 'http://54.226.146.86/api',
+  apiUrl: 'http://54.226.146.86:8080/api', //'http://54.226.146.86/api',
   appName: 'School Management System',
   version: '1.0.0',
   enableDebugTools: false,  // Disable debug tools in production
